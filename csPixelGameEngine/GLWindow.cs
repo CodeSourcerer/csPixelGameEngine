57022eb0b39721a41b4fb33a6209f2d2e8d6cbd6
Chad Strawinski <Chad.Strawinski@coxautoinc.com>
2019-09-23 13:14:58 -0500

    Add GLWindow class

---
+++ csPixelGameEngine/GLWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace csPixelGameEngine
+{
+    public class GLWindow : GameWindow
+    {
+        public GLWindow(int width, int height, string title)
+            : base(width, height, GraphicsMode.Default, title)
+        {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            var input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+    }
+}
