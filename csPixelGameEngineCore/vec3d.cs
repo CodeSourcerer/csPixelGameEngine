555c3e40412390ccab51f4fc7f0f7ba5eaa84198
Chad Strawinski <chad@simpleltc.com>
2020-01-20 11:18:17 -0600

    Added vec2d and vec3d classes

---
+++ csPixelGameEngineCore/vec3d.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPixelGameEngineCore
+{
+    public class vec3d
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public vec3d(float x = 0, float y = 0, float z = 0, float w = 1)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+    }
+}
