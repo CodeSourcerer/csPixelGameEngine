e8ebee16ab2ad72185f11b50da86cd1ee8a9b780
Chad Strawinski <chad@simpleltc.com>
2020-02-11 19:11:02 -0600

    Sprite class

---
+++ csPixelGameEngineCore/Sprite.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csPixelGameEngineCore
+{
+    /// <summary>
+    /// A 2D drawing surface.
+    /// </summary>
+    public class Sprite
+    {
+        public uint Width { get; private set; }
+        public uint Height { get; private set; }
+        public Pixel[] ColorData { get; private set; }
+
+        private Sprite()
+        {
+            // Don't allow creation of sprites without dimensions
+        }
+        
+        public Sprite(uint w, uint h)
+        {
+            if (w == 0)
+                throw new ArgumentException("Argument must be greater than 0", nameof(w));
+            if (h == 0)
+                throw new ArgumentException("Argument must be greater than 0", nameof(h));
+
+            Width = w;
+            Height = h;
+            ColorData = new Pixel[Width * Height];
+
+            for (int i = 0; i < Width * Height; i++)
+            {
+                ColorData[i] = new Pixel();
+            }
+        }
+
+        public Pixel GetPixel(uint x, uint y)
+        {
+            if (x < Width && y < Height)
+                return ColorData[y * Width + x];
+            else
+                return Pixel.BLANK;
+        }
+
+        public bool SetPixel(uint x, uint y, Pixel p)
+        {
+            if (x < Width && y < Height)
+            {
+                ColorData[y * Width + x] = p;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void FillRect(uint x, uint y, uint width, uint height, Pixel p)
+        {
+            if (p == default)
+                p = Pixel.WHITE;
+
+            uint x2 = x + width;
+            uint y2 = y + height;
+
+            if (x > Width)
+                x = Width;
+            if (y > Height)
+                y = Height;
+            if (x2 >= Width)
+                x2 = Width;
+            if (y2 >= Height)
+                y2 = Height;
+
+            for (uint x1 = x; x1 < x2; x1++)
+            {
+                for (uint y1 = y; y1 < y2; y1++)
+                {
+                    ColorData[y1 * Width + x1] = p;
+                }
+            }
+        }
+    }
+}
---
+++ csPixelGameEngineCoreTests/SpriteTests.cs
@@ -0,0 +1,64 @@
+﻿using csPixelGameEngineCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csPixelGameEngineCoreTests
+{
+    [TestClass]
+    public class SpriteTests
+    {
+        [DataTestMethod]
+        [DataRow(0, 0)]
+        [DataRow(0, 1)]
+        [DataRow(1, 0)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreatingSprite_WithZeroXorY_IsNotValid(uint w, uint h)
+        {
+            // Below should throw ArgumentException
+            Sprite testSprite = new Sprite(w, h);
+        }
+
+        [DataTestMethod]
+        [DataRow((uint)1, (uint)1, 1)]
+        [DataRow((uint)10, (uint)10, 100)]
+        public void CreatingSprite_WithNonZeroArguments_InitsColorDataToCorrectSize(uint w, uint h, int expectedSize)
+        {
+            Sprite testSprite = new Sprite(w, h);
+
+            int actualSize = testSprite.ColorData.Length;
+
+            Assert.AreEqual(expectedSize, actualSize, "Size of sprite ColorData array is invalid");
+        }
+
+        [TestMethod]
+        public void CreatingSprite_WithNonZeroArguments_InitsColorDataToPixels()
+        {
+            Sprite testSprite = new Sprite(10, 10);
+
+            bool hasNullPixel = testSprite.ColorData.Any(p => p == null);
+
+            Assert.IsFalse(hasNullPixel, "ColorData not fully initialized with Pixels");
+        }
+
+        [DataTestMethod]
+        [DataRow((uint)0, (uint)0)]
+        [DataRow((uint)9, (uint)9)]
+        public void GetPixel_WithValidCoordinates_ReturnsPixel(uint x, uint y)
+        {
+            Sprite testSprite = new Sprite(10, 10);
+
+            for (uint x1 = 0; x1 < 10; x1++)
+            {
+                for (uint y1 = 0; y1 < 10; y1++)
+                    testSprite.SetPixel(x1, y1, new Pixel(0, 0, (byte)(y1 * 10 + x1)));
+            }
+
+            Pixel actualPixel = testSprite.GetPixel(x, y);
+
+            Assert.AreNotEqual(Pixel.BLANK, actualPixel, "Invalid pixel returned");
+        }
+    }
+}
