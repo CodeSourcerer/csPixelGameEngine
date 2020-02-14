6c972ecb143a76f83118343f7c3e76b93b19bae5
Chad Strawinski <codesourcerer@gmail.com>
2020-02-13 21:09:05 -0600

    Implemented PixelGameEngine class.

---
+++ csPixelGameEngineCore/FrameUpdateEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csPixelGameEngineCore
+{
+    public delegate void FrameUpdateEventHandler(object sender, FrameUpdateEventArgs frameUpdateArgs);
+
+    public class FrameUpdateEventArgs : EventArgs
+    {
+        public double ElapsedTime { get; private set; }
+
+        public FrameUpdateEventArgs(double elapsed)
+            : base()
+        {
+            ElapsedTime = elapsed;
+        }
+    }
+}
