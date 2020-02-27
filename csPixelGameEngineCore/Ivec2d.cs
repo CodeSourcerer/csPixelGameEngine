616eba8720d9eab826a8975a678251d3c2f2e261
Chad Strawinski <chad@simpleltc.com>
2020-02-27 13:03:13 -0600

    Update vec2d struct, add interface.

---
+++ csPixelGameEngineCore/Ivec2d.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csPixelGameEngineCore
+{
+    public interface Ivec2d<T>
+    {
+        float x { get; set; }
+        float y { get; set; }
+
+        T mag();
+        T mag2();
+        Ivec2d<T> norm();
+        Ivec2d<T> perp();
+        T dot(Ivec2d<T> rhs);
+        T cross(Ivec2d<T> rhs);
+
+        public T this[int i] { get; set; }
+    }
+}
