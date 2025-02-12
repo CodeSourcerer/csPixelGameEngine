f3a4a8266babf09aa25c0ef3a0857e010e782e93
Chad Strawinski <codesourcerer@gmail.com>
2025-02-11 22:39:58 -0600

    Refactor the vector types.

---
+++ csPixelGameEngineCore/v_2d.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPixelGameEngineCore;
+
+public abstract class v_2d<T> : IEquatable<v_2d<T>>, IComparable<v_2d<T>> where T : struct
+{
+    public T x = default(T);
+    public T y = default(T);
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public v_2d() { }
+
+    /// <summary>
+    /// Specific constructor
+    /// </summary>
+    /// <param name="_x"></param>
+    /// <param name="_y"></param>
+    public v_2d(T _x, T _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    /// <summary>
+    /// Copy constructor
+    /// </summary>
+    /// <param name="v"></param>
+    public v_2d(v_2d<T> v)
+    {
+        x = v.x;
+        y = v.y;
+    }
+
+    /// <summary>
+    /// Returns rectangular area of vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract T area();
+
+    /// <summary>
+    /// Returns magnitude of vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract T mag();
+
+    /// <summary>
+    /// Returns magnitude squared of vector (useful for fast comparisons)
+    /// </summary>
+    /// <returns></returns>
+    public abstract T mag2();
+
+    /// <summary>
+    /// Returns normalised version of vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> norm();
+
+    /// <summary>
+    /// Returns vector at 90 degrees to this one
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> perp();
+
+    /// <summary>
+    /// Rounds both components down
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> floor();
+
+    /// <summary>
+    /// Rounds both components up
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> ceil();
+
+    /// <summary>
+    /// Returns 'element-wise' max of this and another vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> max(v_2d<T> v);
+
+    /// <summary>
+    /// Returns 'element-wise' min of this and another vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> min(v_2d<T> v);
+
+    /// <summary>
+    /// Calculates scalar dot product between this and another vector
+    /// </summary>
+    /// <returns></returns>
+    public abstract T dot(v_2d<T> rhs);
+
+    /// <summary>
+    /// Calculates 'scalar' cross product between this and another vector (useful for winding orders)
+    /// </summary>
+    /// <returns></returns>
+    public abstract T cross(v_2d<T> rhs);
+
+    /// <summary>
+    /// Treat this as polar coordinate (R, Theta), return cartesian equivalent (X, Y)
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> cart();
+
+    /// <summary>
+    /// Treat this as cartesian coordinate (X, Y), return polar equivalent (R, Theta)
+    /// </summary>
+    /// <returns></returns>
+    public abstract v_2d<T> polar();
+
+    /// <summary>
+    /// Clamp the components of this vector in between the 'element-wise' minimum and maximum of 2 other vectors
+    /// </summary>
+    /// <param name="v1"></param>
+    /// <param name="v2"></param>
+    /// <returns></returns>
+    public abstract v_2d<T> clamp(v_2d<T> v1, v_2d<T> v2);
+
+    /// <summary>
+    /// Linearly interpolate between this vector, and another vector, given normalised parameter 't'
+    /// </summary>
+    /// <param name="v1"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public abstract v_2d<T> lerp(v_2d<T> v1, double t);
+
+    /// <summary>
+    /// Assuming this vector is incident, given a normal, return the reflection
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public abstract v_2d<T> reflect(v_2d<T> n);
+
+    public abstract v_2d<T> mult(v_2d<T> rhs);
+    public abstract v_2d<T> mult(T rhs);
+    public abstract v_2d<T> div(v_2d<T> rhs);
+    public abstract v_2d<T> div(T rhs);
+    public abstract v_2d<T> sum(v_2d<T> rhs);
+    public abstract v_2d<T> sum(T rhs);
+    public abstract v_2d<T> neg();
+
+    /// <summary>
+    /// Compare if this vector is numerically equal to another
+    /// </summary>
+    /// <param name="lhs"></param>
+    /// <returns></returns>
+    public abstract bool Equals(v_2d<T> lhs);
+
+    public override bool Equals(object lhs) => Equals(lhs as v_2d<T>);
+
+    public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode();
+
+    public abstract int CompareTo(v_2d<T> rhs);
+
+    /// <summary>
+    /// Compare if this vector is numerically equal to another
+    /// </summary>
+    /// <param name="lhs"></param>
+    /// <param name="rhs"></param>
+    /// <returns></returns>
+    public static bool operator ==(v_2d<T> lhs, v_2d<T> rhs) => lhs.Equals(rhs);
+
+    /// <summary>
+    /// Compare if this vector is not numerically equal to another
+    /// </summary>
+    /// <param name="lhs"></param>
+    /// <param name="rhs"></param>
+    /// <returns></returns>
+    public static bool operator !=(v_2d<T> lhs, v_2d<T> rhs) => !lhs.Equals(rhs);
+
+    public static v_2d<T> operator *(v_2d<T> lhs, v_2d<T> rhs) => lhs.mult(rhs);
+    public static v_2d<T> operator *(T lhs, v_2d<T> rhs) => rhs.mult(lhs);
+    public static v_2d<T> operator *(v_2d<T> lhs, T rhs) => lhs.mult(rhs);
+
+    public static v_2d<T> operator /(v_2d<T> lhs, v_2d<T> rhs) => lhs.div(rhs);
+    public static v_2d<T> operator /(T lhs, v_2d<T> rhs) => rhs.div(lhs);
+    public static v_2d<T> operator /(v_2d<T> lhs, T rhs) => lhs.div(rhs);
+
+    public static v_2d<T> operator +(v_2d<T> lhs, v_2d<T> rhs) => lhs.sum(rhs);
+    public static v_2d<T> operator +(T lhs, v_2d<T> rhs) => rhs.sum(lhs);
+    public static v_2d<T> operator +(v_2d<T> lhs, T rhs) => lhs.sum(rhs);
+
+    public static v_2d<T> operator -(v_2d<T> rhs) => rhs.neg();
+    public static v_2d<T> operator -(v_2d<T> lhs, v_2d<T> rhs) => lhs.sum(-rhs);
+    public static v_2d<T> operator -(T lhs, v_2d<T> rhs) => rhs.neg().sum(lhs);
+    public static v_2d<T> operator -(v_2d<T> lhs, T rhs) => lhs.neg().sum(rhs);
+
+    public static bool operator <(v_2d<T> lhs, v_2d<T> rhs) => lhs.CompareTo(rhs) < 0;
+    public static bool operator >(v_2d<T> lhs, v_2d<T> rhs) => lhs.CompareTo(rhs) > 0;
+}
---
+++ csPixelGameEngineCore/vf2d.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace csPixelGameEngineCore;
+
+public class vf2d : v_2d<float>
+{
+    public vf2d() : base(0.0f, 0.0f) { }
+    public vf2d(float x, float y) : base(x, y) { }
+
+    public override float area() => x * y;
+    public override float mag() => (float)Math.Sqrt(x * x + y * y);
+    public override float mag2() => x * x + y * y;
+    public override v_2d<float> norm()
+    {
+        float r = 1 / mag();
+        return new vf2d(r * r, y * r);
+    }
+    public override vf2d perp() => new vf2d(-y, x);
+    public override vf2d floor() => new vf2d((float)Math.Floor(x), (float)Math.Floor(y));
+    public override vf2d ceil() => new vf2d((float)Math.Ceiling(x), (float)Math.Ceiling(y));
+    public override vf2d max(v_2d<float> v) => new vf2d(Math.Max(x, v.x), Math.Max(y, v.y));
+    public override vf2d min(v_2d<float> v) => new vf2d(Math.Min(x, v.x), Math.Min(y, v.y));
+    public override float dot(v_2d<float> rhs) => x * rhs.x + y * rhs.y;
+    public override float cross(v_2d<float> rhs) => x * rhs.y - y * rhs.x;
+    public override vf2d cart() => new vf2d((float)(Math.Cos(y) * x), (float)(Math.Sin(y) * x));
+    public override vf2d polar() => new vf2d(mag(), (float)Math.Atan2(y, x));
+    public override vf2d clamp(v_2d<float> v1, v_2d<float> v2) => max(v1).min(v2);
+    public override v_2d<float> lerp(v_2d<float> v1, double t) => this * ((float)(1.0 - t)) + (v1 * (float)t);
+    public override v_2d<float> reflect(v_2d<float> n) => this - 2 * dot(n) * n;
+
+    public override v_2d<float> mult(v_2d<float> rhs) => new vf2d(x * rhs.x, y * rhs.y);
+    public override v_2d<float> mult(float rhs) => new vf2d(x * rhs, y * rhs);
+    public override v_2d<float> div(v_2d<float> rhs) => new vf2d(x / rhs.x, y / rhs.y);
+    public override v_2d<float> div(float rhs) => new vf2d(x / rhs, y / rhs);
+    public override v_2d<float> sum(v_2d<float> rhs) => new vf2d(x + rhs.x, y + rhs.y);
+    public override v_2d<float> sum(float rhs) => new vf2d(x + rhs, y + rhs);
+    public override v_2d<float> neg() => new vf2d(-x, -y);
+
+    public override bool Equals(v_2d<float> lhs) => x == lhs.x && y == lhs.y;
+
+    public override int CompareTo(v_2d<float> rhs) => y == rhs.y ? (int)(y - rhs.y) : (int)(x - rhs.x);
+}
