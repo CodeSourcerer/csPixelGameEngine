914544cbe3e12c033a7f511b15d5c197e2cad678
Chad Strawinski <codesourcerer@gmail.com>
2025-06-16 23:26:32 -0500

    Add GPUTasks to LayerDesc struct

--- csPixelGameEngineCore/LayerDesc.cs
+++ csPixelGameEngineCore/LayerDesc.cs
@@ -5,13 +5,15 @@ namespace csPixelGameEngineCore;
 
 public class LayerDesc(IRenderer renderer)
 {
-    public vf2d       vOffset    { get; set; } = new vf2d();
-    public vf2d       vScale     { get; set; } = new vf2d(1.0f, 1.0f);
+    public vf2d       vOffset    { get; set; } = new ();
+    public vf2d       vScale     { get; set; } = new (1.0f, 1.0f);
     public bool       bShow      { get; set; } = false;
     public bool       bUpdate    { get; set; } = false;
-    public Renderable DrawTarget { get; set; } = new Renderable(renderer);
+    public Renderable DrawTarget { get; set; } = new (renderer);
     public uint       ResID      { get; set; } = 0;
     public Pixel      Tint       { get; set; } = Pixel.WHITE;
     public Action     funcHook   { get; set; }
-    public List<DecalInstance> DecalInstance { get; } = new List<DecalInstance>();
+
+    public List<DecalInstance> DecalInstance { get; init; } = [];
+    public List<GPUTask>       GPUTasks      { get; init; } = [];
 }
